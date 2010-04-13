b84791ad417df3ed05fb2f82a59719acbab99951
gaelle <gaelle@uneek-gaelle.(none)>
2010-04-13 10:01:22 +0200

    correction buf sous type machines

--- app/models/tool_subtype.rb
+++ app/models/tool_subtype.rb
@@ -1,2 +1,9 @@
 class ToolSubtype < ActiveRecord::Base
+
+
+  named_scope :for_test_engines, :conditions => ["tool_type = ?", "TestEngine"]
+  named_scope :for_tool_engines, :conditions => ["tool_type = ?", "ToolEngine"]
+  named_scope :for_softwares, :conditions => ["tool_type = ?", "Software"]
+  named_scope :for_measuring_engines, :conditions => ["tool_type = ?", "MeasuringEngine"]
+
 end
