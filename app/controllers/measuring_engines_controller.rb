86312f86d50613a86c0bb13d2c725903a164bd1a
gaelle <gaelle@uneek-gaelle.(none)>
2010-05-07 12:54:29 +0200

    remise à zéro filtres lors du changement de type doutil

--- app/controllers/measuring_engines_controller.rb
+++ app/controllers/measuring_engines_controller.rb
@@ -1,6 +1,17 @@
 class MeasuringEnginesController < SiteController
  
-  
+
+  def index
+    @search = Tool.search(params[:search])
+    @tools = @search.paginate(:all,:page => params[:page], :per_page => 30, :order => "name ASC")
+
+    @tool_type = "MeasuringEngine"
+    save_tools_collection @search.map{|t| t.id}
+    save_search_params params[:search]
+
+    radiant_render :page => "/tools"
+  end
+
   def show
     @measuring_engine = MeasuringEngine.find(params[:id])
     save_tool_show @measuring_engine.id
@@ -18,6 +29,15 @@ class MeasuringEnginesController < SiteController
 
   def save_tool_show tool_id
     session[:tool_show] = tool_id
+  end
+
+  def save_tools_collection collection
+    session[:tools_collection] = collection
+  end
+
+
+  def save_search_params search_params
+    session[:search_params] = search_params
   end
 
   
--- app/controllers/test_engines_controller.rb
+++ app/controllers/test_engines_controller.rb
@@ -1,6 +1,17 @@
 class TestEnginesController < SiteController
  
-  
+
+  def index
+    @search = Tool.search(params[:search])
+    @tools = @search.paginate(:all,:page => params[:page], :per_page => 30, :order => "name ASC")
+
+    @tool_type = "TestEngine"
+    save_tools_collection @search.map{|t| t.id}
+    save_search_params params[:search]
+
+    radiant_render :page => "/tools"
+  end
+
  def show
     @test_engine = TestEngine.find(params[:id])
     save_tool_show @test_engine.id
@@ -18,6 +29,15 @@ class TestEnginesController < SiteController
 
   def save_tool_show tool_id
     session[:tool_show] = tool_id
+  end
+
+  def save_tools_collection collection
+    session[:tools_collection] = collection
+  end
+
+
+  def save_search_params search_params
+    session[:search_params] = search_params
   end
 
   
