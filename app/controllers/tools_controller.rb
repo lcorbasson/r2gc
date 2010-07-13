4a47f430b9c59fe358caf3b8227ce12d89898377
Gaelle <favreau.gaelle@gmail.com>
2010-07-13 15:31:07 +0200

    soucis export excel

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -6,7 +6,7 @@ class ToolsController < SiteController
     @search = Tool.search(params[:search])
     @tools = @search.paginate(:all, :page => params[:page], :per_page => 30, :order => "tools.name ASC")
     @export_tools = @search.paginate(:all,:page => 1, :per_page => @tools.total_pages*30>0 ? @tools.total_pages*30 : 1 , :order => "tools.name ASC")
-    
+   
     @tool_type = ""
      if params[:search] && !params[:search][:laboratory_id_equals].blank?
       if !params[:search][:correspondents_search_entity_id_equals].blank?
