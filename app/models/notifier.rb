c7c27045ad3ea859e74fd9623c0c6454413bf47b
Gaelle <favreau.gaelle@gmail.com>
2010-07-15 10:50:24 +0200

    emails

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -32,7 +32,7 @@ class Notifier < ActionMailer::Base
   from       "contact@pole-gce.fr"
   body :user => user, :password => password
 
-   attachment :content_type => "application/octet-stream", :body => File.read("#{RAILS_ROOT}/public/documents/informations.doc")
+   attachment :content_type => "application/octet-stream", :body => File.read("#{RAILS_ROOT}/public/documents/informations.doc"), :name => "informations.doc"
   attachment :content_type => "application/excel", :body => File.read("#{RAILS_ROOT}/public/documents/statuts.xls")
  end
 
