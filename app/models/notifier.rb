6e8a0de638dd743c4e4a5fd9616ef92bf47b0841
Gaelle <favreau.gaelle@gmail.com>
2010-07-15 10:56:18 +0200

    emails

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -5,7 +5,8 @@ class Notifier < ActionMailer::Base
     recipients recipients
     from sender_email
     subject "Base de données équipement: Demande d'informations"
-    content_type = "text/html"
+    content_type "text/html"
+
 
     body :content => content, :sender_name => sender_name, :email => sender_email, :tool_name => tool_name
   end
@@ -14,7 +15,8 @@ class Notifier < ActionMailer::Base
     subject    "Réinitialisation de votre mot de passe pour la base de données équipements"
     recipients user.email
     from       "contact@pole-gce.fr"
-    content_type = "text/html"
+    content_type "text/html"
+
 
     body :user => user, :password => password
   end
@@ -23,7 +25,8 @@ class Notifier < ActionMailer::Base
   subject    "Mise en ligne de la base de données équipements"
   recipients user.email
   from       "contact@pole-gce.fr"
-  content_type = "text/html"
+  content_type "text/html"
+
 
   body :user => user, :password => password
 
@@ -36,7 +39,8 @@ class Notifier < ActionMailer::Base
   subject    "Mise en ligne de la base de données équipements"
   recipients user.email
   from       "contact@pole-gce.fr"
-  content_type = "text/html"
+  content_type "text/html"
+
 
   body :user => user, :password => password
 
@@ -49,7 +53,8 @@ class Notifier < ActionMailer::Base
   recipients user.email
   from       "contact@pole-gce.fr"
   body :user => user, :password => password
-  content_type = "text/html"
+  content_type "text/html"
+
 
 
   attachment :content_type => "application/octet-stream", :body => File.read("#{RAILS_ROOT}/public/documents/informations.doc")
@@ -62,7 +67,8 @@ class Notifier < ActionMailer::Base
   recipients user.email
   from       "contact@pole-gce.fr"
   body :user => user, :password => password
-  content_type = "text/html"
+  content_type "text/html"
+
 
 
   attachment :content_type => "application/octet-stream", :body => File.read("#{RAILS_ROOT}/public/documents/informations.doc")
@@ -75,7 +81,8 @@ class Notifier < ActionMailer::Base
   recipients user.email
   from       "contact@pole-gce.fr"
   body :user => user
-  content_type = "text/html"
+  content_type "text/html"
+
 
 
  end
@@ -85,7 +92,8 @@ class Notifier < ActionMailer::Base
   recipients user.email
   from       "contact@pole-gce.fr"
   body :user => user
-  content_type = "text/html"
+  content_type "text/html"
+
 
 
  end
@@ -95,7 +103,8 @@ class Notifier < ActionMailer::Base
   recipients user.email
   from       "contact@pole-gce.fr"
   body :user => user
-  content_type = "text/html"
+  content_type "text/html"
+
 
 
  end
